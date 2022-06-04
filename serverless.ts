d969307995c824b6d78c9f70345f3754a638ba82
omenocal <octavio_menocal92@hotmail.es>
2022-06-03 19:32:30 -0600

    Setup DynamoDB table

--- serverless.ts
+++ serverless.ts
@@ -1,14 +1,12 @@
 import type { AWS } from '@serverless/typescript';
-
-import hello from '@functions/hello';
-
+import { createReservation } from '@functions/reservation';
 const serverlessConfiguration: AWS = {
   service: 'aws-serverless-typescript-api',
   frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  plugins: ['serverless-esbuild', 'serverless-offline', 'serverless-dynamodb-local'],
   provider: {
     name: 'aws',
-    runtime: 'nodejs14.x',
+    runtime: 'nodejs16.x',
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
@@ -17,22 +15,66 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
+    iam: {
+      role: {
+        statements: [{
+          Effect: "Allow",
+          Action: [
+            "dynamodb:Query",
+            "dynamodb:GetItem",
+            "dynamodb:PutItem",
+            "dynamodb:UpdateItem",
+            "dynamodb:DeleteItem",
+          ],
+          Resource: "arn:aws:dynamodb:us-west-2:*:table/ReservationTable",
+        }],
+      },
+    },
   },
   // import the function via paths
-  functions: { hello },
+  functions: { createReservation },
   package: { individually: true },
-  custom: {
+  custom:{
     esbuild: {
       bundle: true,
       minify: false,
       sourcemap: true,
       exclude: ['aws-sdk'],
-      target: 'node14',
+      target: 'node16',
       define: { 'require.resolve': undefined },
       platform: 'node',
       concurrency: 10,
     },
+    dynamodb:{
+      start:{
+        port: 5000,
+        inMemory: true,
+        migrate: true,
+      },
+      stages: "dev"
+    }
+  },
+  resources: {
+    Resources: {
+      ReservationTable: {
+        Type: "AWS::DynamoDB::Table",
+        Properties: {
+          TableName: "ReservationTable",
+          AttributeDefinitions: [{
+            AttributeName: "reservationId",
+            AttributeType: "S",
+          }],
+          KeySchema: [{
+            AttributeName: "reservationId",
+            KeyType: "HASH"
+          }],
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1
+          },
+        },
+      },
+    },
   },
 };
-
 module.exports = serverlessConfiguration;
