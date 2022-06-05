3b241338d82e48b3006cf30ef7511097b7885972
omenocal <octavio_menocal92@hotmail.es>
2022-06-05 11:40:16 -0600

    Return application/json responses

--- src/libs/api-gateway.ts
+++ src/libs/api-gateway.ts
@@ -7,6 +7,9 @@ export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayP
 export const formatJSONResponse = (response: Record<string, unknown>) => {
   return {
     statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify(response),
   };
 };
