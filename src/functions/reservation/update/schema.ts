901dfb63aa13b1a6eb053488e7fccea3addfa540
omenocal <octavio_menocal92@hotmail.es>
2022-06-05 11:30:29 -0600

    Define lambda serverless config individually

--- src/functions/reservation/update/schema.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  type: "object",
-  properties: {
-    roomId: { type: 'string' },
-    clientId: { type: 'string' },
-    startDate: { type: 'string' },
-    endDate: { type: 'string' },
-  },
-} as const;
